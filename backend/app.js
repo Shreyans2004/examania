59c68a62cd224af53b68a2d2cec522a8df1c7ff2
Akash Paloju <akashpaloju@gmail.com>
2023-10-25 10:37:38 +0530

    chore:created server and connected to DB

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,27 @@
 const express = require("express");
 const mysql = require("mysql");
 const cors = require("cors");
-
+const morgan = require("morgan");
+const dbConfig = require("./dbConfig");
+const port = process.env.PORT || 8000 ;
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
 
+const db = mysql.createConnection({
+  host: dbConfig.host,
+  user: dbConfig.user,
+  password: dbConfig.password,
+  database: dbConfig.database
+})
+
+app.get("/", (req,res) => {
+  res.json({message: "Welcome to the Examania application."});
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
