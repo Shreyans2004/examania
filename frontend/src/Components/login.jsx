e120e362207d2a7b0818da7815ec96c482ea6bd1
Akash Paloju <119884665+AkashPaloju@users.noreply.github.com>
2023-10-25 16:28:58 +0530

    refactor : updating names to login

--- frontend/src/Components/login.jsx
+++ frontend/src/Components/login.jsx
@@ -1,7 +1,7 @@
 import Navbar from "./Navbar.js";
 import Hero from "./Hero.js";
-import "./Body1.css"
-function Body1() {
+import "./login.css"
+function Login() {
   return (
     <>
     <div className="div_main">
@@ -14,4 +14,4 @@ function Body1() {
   );
 }
 
-export default Body1;
+export default Login;
